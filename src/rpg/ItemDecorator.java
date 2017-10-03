f757754fcb61b336210ba13ae31548485e7198d2
jorge <jamc4626@gmail.com>
2017-10-03 15:05:51 -0300

    diagrama actualizado

--- src/rpg/ItemDecorator.java
+++ src/rpg/ItemDecorator.java
@@ -3,4 +3,9 @@ public abstract class ItemDecorator {
 
 	private Item item;
 
+	public ItemDecorator(Item item) {
+		this.item = item;
+	}
+
+	//para agregar metodos futuros a los items
 }
--- src/rpg/UnidadDecorator.java
+++ src/rpg/UnidadDecorator.java
@@ -3,4 +3,10 @@ public abstract class UnidadDecorator {
 
 	private Unidad unidad;
 
+	public UnidadDecorator(Unidad unidad) {
+		this.unidad = unidad;
+	}
+	
+	//para agregar metodos futuros a las unidades
+
 }
